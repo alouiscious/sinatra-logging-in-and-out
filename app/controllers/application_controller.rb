5cc76d023c5bae136fe2cf3b520be9df7f209c27
Alouiscious Media <info@reneesoul.com>
2019-10-27 17:30:38 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,10 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    # binding.pry
   
     @user = User.find_by(username: params[:username])
       if @user != nil && @user.password == params[:password]
-        session[:user_id] = @user.id 
+        # session[:user_id] = @user.id 
         redirect to '/account'
       end
 
